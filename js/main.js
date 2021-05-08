136e11628ed5a007041491b14e9b18a3e8e22afb
Dimi <triotaxa@hotmail.com>
2021-05-08 13:28:03 +0200

    js-sum-variables

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 10;
+
+let secondNumber = 20;
+
+function add(firstNumber, secondNumber){
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+console.log(add(firstNumber,secondNumber));
